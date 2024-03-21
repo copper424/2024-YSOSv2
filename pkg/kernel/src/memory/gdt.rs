1559785c06cdffbfcccca1673c9d387857378026
copper424 <wy1064299135@outlook.com>
2024-03-21 15:47:28 +0800

    build : bump x86_64 crate to 0.15 with modifications from lab2

--- pkg/kernel/src/memory/gdt.rs
+++ pkg/kernel/src/memory/gdt.rs
@@ -20,7 +20,7 @@ lazy_static! {
             const STACK_SIZE: usize = IST_SIZES[0];
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
             let stack_start = VirtAddr::from_ptr(unsafe { STACK.as_ptr() });
-            let stack_end = stack_start + STACK_SIZE;
+            let stack_end = stack_start + STACK_SIZE as u64;
             info!(
                 "Privilege Stack  : 0x{:016x}-0x{:016x}",
                 stack_start.as_u64(),
@@ -35,7 +35,7 @@ lazy_static! {
             const STACK_SIZE: usize = IST_SIZES[1];
             static mut STACK: [u8;STACK_SIZE] = [0;STACK_SIZE];
             let stack_start = VirtAddr::from_ptr(unsafe { STACK.as_ptr() });
-            let stack_end = stack_start + STACK_SIZE;
+            let stack_end = stack_start + STACK_SIZE as u64;
             info!(
                 "Double Fault Stack for Interruption : 0x{:016x}-0x{:016x}",
                 stack_start.as_u64(),
@@ -47,7 +47,7 @@ lazy_static! {
             const STACK_SIZE: usize = IST_SIZES[2];
             static mut STACK: [u8;STACK_SIZE] = [0;STACK_SIZE];
             let stack_start = VirtAddr::from_ptr(unsafe { STACK.as_ptr() });
-            let stack_end = stack_start + STACK_SIZE;
+            let stack_end = stack_start + STACK_SIZE as u64;
             info!(
                 "Page Fault Stack for Interruption : 0x{:016x}-0x{:016x}",
                 stack_start.as_u64(),
@@ -59,7 +59,7 @@ lazy_static! {
             const STACK_SIZE: usize = IST_SIZES[3];
             static mut STACK: [u8;STACK_SIZE] = [0;STACK_SIZE];
             let stack_start = VirtAddr::from_ptr(unsafe { STACK.as_ptr() });
-            let stack_end = stack_start + STACK_SIZE;
+            let stack_end = stack_start + STACK_SIZE as u64;
             info!(
                 "Clock Interruption Stack for Interruption : 0x{:016x}-0x{:016x}",
                 stack_start.as_u64(),
@@ -74,9 +74,9 @@ lazy_static! {
 lazy_static! {
     static ref GDT: (GlobalDescriptorTable, KernelSelectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
-        let data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
-        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        let code_selector = gdt.append(Descriptor::kernel_code_segment());
+        let data_selector = gdt.append(Descriptor::kernel_data_segment());
+        let tss_selector = gdt.append(Descriptor::tss_segment(&TSS));
         (
             gdt,
             KernelSelectors {
