09c30499158636d119aeacbc2b73d84cedcd63f7
copper424 <wy1064299135@outlook.com>
2024-03-25 21:56:28 +0800

    feat(serial): change the behaviour of UART driver to support parsing of input from UTF-8 character

--- pkg/kernel/src/interrupt/serial.rs
+++ pkg/kernel/src/interrupt/serial.rs
@@ -2,7 +2,7 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 use super::consts::*;
 use crate::drivers::input;
-use crate::serial::get_serial;
+use crate::serial::get_serial_for_sure;
 
 pub unsafe fn register_idt(idt: &mut InterruptDescriptorTable) {
     idt[Interrupts::IrqBase as u8 + Irq::Serial0 as u8].set_handler_fn(serial_handler);
@@ -17,10 +17,16 @@ pub extern "x86-interrupt" fn serial_handler(_st: InterruptStackFrame) {
 /// Should be called on every interrupt
 fn receive() {
     // FIXME: receive character from uart 16550, put it into INPUT_BUFFER
-    if let Some(key) = get_serial().unwrap().receive() {
-        debug!("Receive key: {:?}\n", key);
-        input::push_key(key as char);
-    }else{
-        debug!("Do not receive key\n");
+    let mut char_buf = [0u8; 4];
+    for idx in 0..4 {
+        if let Some(byte) = get_serial_for_sure().receive() {
+            char_buf[idx] = byte;
+            if let Ok(key) = core::str::from_utf8(&char_buf[0..idx + 1]) {
+                input::push_key(key.chars().next().unwrap());
+                break;
+            }
+        } else {
+            break;
+        }
     }
 }
