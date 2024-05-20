1268d9d54fca08d8938e76ec52528b41582efab0
copper424 <wy1064299135@outlook.com>
2024-05-20 10:22:19 +0800

    Refactor implementation of philosopher dinner problem

--- pkg/app/dinner/src/main.rs
+++ pkg/app/dinner/src/main.rs
@@ -8,41 +8,76 @@ fn main() -> isize {
     for i in 0..CHOPSTICK.len() {
         CHOPSTICK[i].init(1);
     }
+    philosopher(0);
+    0
+}
+
+fn philosopher(choice: i32) {
     let mut pids = [0; 5];
-    for i in 0..CHOPSTICK.len() {
-        let pid = sys_fork();
-        if pid == 0 {
-            philosopher(i, &CHOPSTICK);
-            sys_exit(0);
-        } else {
-            pids[i] = pid as u16;
+
+    match choice {
+        0 => {
+            for i in 0..CHOPSTICK.len() {
+                let pid = sys_fork();
+                if pid == 0 {
+                    philosopher1(i);
+                    sys_exit(0);
+                } else {
+                    pids[i] = pid as u16;
+                }
+            }
         }
+        1 => {
+            let avail = Semaphore::new(5);
+            avail.init(CHOPSTICK.len() - 1);
+            for i in 0..CHOPSTICK.len() {
+                let pid = sys_fork();
+                if pid == 0 {
+                    philosopher2(i, &avail);
+                    sys_exit(0);
+                } else {
+                    pids[i] = pid as u16;
+                }
+            }
+        }
+        _ => unreachable!("Invalid choice"),
     }
+
     sys_stat();
     for i in 0..CHOPSTICK.len() {
         sys_wait_pid(pids[i]);
     }
-    0
 }
 
-fn philosopher(id: usize, chopsticks: &[Semaphore; 5]) {
+fn philosopher1(id: usize) {
     let left = id;
-    let right = (id + 1) % 5;
+    let right = (id + 1) % CHOPSTICK.len();
     if id & 0x1 == 0 {
-        chopsticks[left].wait();
-        chopsticks[right].wait();
+        CHOPSTICK[left].wait();
+        CHOPSTICK[right].wait();
 
         println!("Philosopher {} is eating", id);
-        chopsticks[right].signal();
-        chopsticks[left].signal();
+        CHOPSTICK[right].signal();
+        CHOPSTICK[left].signal();
     } else {
-        chopsticks[right].wait();
-        chopsticks[left].wait();
+        CHOPSTICK[right].wait();
+        CHOPSTICK[left].wait();
 
         println!("Philosopher {} is eating", id);
-        chopsticks[left].signal();
-        chopsticks[right].signal();
+        CHOPSTICK[left].signal();
+        CHOPSTICK[right].signal();
     }
 }
 
+fn philosopher2(id: usize, avail: &Semaphore) {
+    avail.wait();
+    let left = id;
+    let right = (id + 1) % CHOPSTICK.len();
+    CHOPSTICK[left].wait();
+    CHOPSTICK[right].wait();
+    println!("Philosopher {} is eating", id);
+    CHOPSTICK[right].signal();
+    CHOPSTICK[left].signal();
+    avail.signal();
+}
 entry!(main);
